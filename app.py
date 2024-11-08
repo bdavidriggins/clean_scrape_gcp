ff71995caf42942ed6b53fab1f4550cda6a55b42
bdavidriggins <bdavidriggins@gmail.com>
2024-11-07 20:00:35 -0500

    Trigger deployment to App Engine

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-@app.route('/about')
-def about():
-    return 'About Page'
+    app.run(host='0.0.0.0', port=8080)
